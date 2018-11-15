de7dc831aecb586aa9cc92642b8476dedcc457dd
Kuppuswamy Sathyanarayanan <sathyaosid@gmail.com>
2018-11-14 22:31:49 -0800

    programming: Add alloc support.
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyaosid@gmail.com>

--- C/alloc.c
+++ C/alloc.c
@@ -2,11 +2,35 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define ALIGNMENT	8
+#define ALIGN(size)	(((size) + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1))
+#define BUCKET_SIZE	10
+
 typedef struct mheader {
 	size_t size;
+	unsigned int magic;
+	struct mheader *next;
 } mheader_t;
 
-void *mem_alloc(size_t size)
+size_t hsize = ALIGN(sizeof(mheader));
+
+mheader_t *free_blocks;
+
+void *get_freeblock(size_t size)
 {
-	size_t new_size = ALIGN_UP(size + sizeof(mheader));
+	mheader_t *block = free_blocks;
+
+	while (block) {
+		if (size <= block->size)
+			break;
+		block = block->next;
+	}
+
+	return block
+}
+
+void *memalloc(size_t size)
+{
+	size_t bsize = ALIGN(size + hsize);
+	mheader_t
 }
